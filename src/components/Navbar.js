959ce95b5c6f4f3d54cf48f7124467c437008ae4
watanabk <x25r.ac@gmail.com>
2019-03-12 15:19:50 +0900

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,17 +63,14 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+              <Link className="navbar-item" to="/test">
+                test
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
